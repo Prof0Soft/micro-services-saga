15fa0f879e7e8ad49c991b468a4ed6b0a6b74f4d
SergeyB <s.borsuk@softteco.com>
2022-05-16 18:49:04 +0300

    Add services to project.

---
+++ services-saga-orchestrator/services-saga-orchestrator/src/main/java/com/servicessagaorchestrator/servicessagaorchestrator/ServicesSagaOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.servicessagaorchestrator.servicessagaorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesSagaOrchestratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicesSagaOrchestratorApplication.class, args);
+    }
+
+}
---
+++ services-saga-orchestrator/services-saga-orchestrator/src/test/java/com/servicessagaorchestrator/servicessagaorchestrator/ServicesSagaOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicessagaorchestrator.servicessagaorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesSagaOrchestratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
